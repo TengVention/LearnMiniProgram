a36f40f5ec7c914dd18a2caef97d4c7c6a4af484
tengvention <tengvention@163.com>
2020-10-26 11:15:18 +0800

    注册小程序和页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,47 @@
 // pages/home/home.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    message: '哈哈哈',
+    list: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad() {
+    console.log('onLoad')
+    wx.request({
+      //
+      url: 'http://123.207.32.32:8000/recommend',
+      success: (res) => {
+        console.log(res)
+        // const data = res.data.data.list;
+        // this.setData({
+        //   list: data
+        // })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onShow() {
+    console.log('onShow')
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onHide() {
+    console.log('onHide')
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  onUnload() {
+    console.log('onUnload')
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onReady() {
+    console.log('onReady')
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  handlerViewClick() {
+    console.log("被点击")
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  onPageScroll(obj) {
+    console.log(obj)
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onReachBottom() {
+    console.log("到底了")
+  },
+  onPullDownRefresh() {
+    console.log("下拉刷新")
   }
+
 })
